b02e16258010295fb4aa7bd296c4f775e256bda3
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-20 20:27:49 -0400

    Added outlets and actions

--- MAPD714-VBilyk-Lab03/ViewController.swift
+++ MAPD714-VBilyk-Lab03/ViewController.swift
@@ -4,18 +4,34 @@
 //  StudendID: 300964200
 //  Date: 20 Sep 2017
 //  Description: Intermediate User Interactions Demo
-//  Version: 0.1 - Built Basic UI
+//  Version: 0.2 - Added outlets and actions
 //
 //  Copyright © 2017 Shifty Land LLC. All rights reserved.
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    //OUTLETS +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    //METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //ACTION METHODS ++++++++++++++++++++++++++++++++++++++++++++++++
+    @IBAction func sliderDrag(_ sender: UISlider) {
+    }
+    
+    @IBAction func onSegmentedControlPress(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func onDoSomethingClick(_ sender: UIButton) {
+    }
+    @IBAction func onSwitchToggle(_ sender: UISwitch) {
+    }
 }
 
